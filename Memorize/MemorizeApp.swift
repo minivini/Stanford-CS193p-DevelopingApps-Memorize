e5ac0c84514a5db4b89f6340810ac658f7d58a71
Ofir Ron <ofir@bdapps.net>
2022-03-16 21:05:12 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ofir Ron on 16/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
